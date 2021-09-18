3f4d847786d4b783d6896d5e01bb4787920ef32a
justinsalasdev <justinsalasdev@gmail.com>
2021-09-18 21:27:00 +0800

    savepoint: calendar component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,20 @@
 import { signInWithRedirect } from "firebase/auth";
 import { auth, provider } from "../firebase/firebase";
+import "react-calendar/dist/Calendar.css";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 
 export default function App() {
+  const [value, onChange] = useState(new Date());
   function handleLogin() {
     signInWithRedirect(auth, provider);
   }
   return (
     <div>
-      hahahaha
-      <button onClick={handleLogin}>signin</button>
+      <Calendar onChange={onChange} value={value} />
+      <button onClick={handleLogin} className="bg-red-400">
+        signin
+      </button>
     </div>
   );
 }
